a3f58c4eafc4bcbcf16f32a53fd384518b9e5127
Albert Astals Cid <tsdgeos@terra.es>
2009-02-01 19:55:40 +0000

    revert parts of r918686, commit by annma from a patch by Thomas Richard
    It changes licenses from GPLv2 to GPLv2+ and adds license headers to some files.
    
    I'm reverting this until we find out if Thomas contacted the proper authors and they agreed or if it was just a newbie mistake
    
    
    svn path=/trunk/KDE/kdeaccessibility/kmousetool/; revision=919889

--- kmousetool/version.h
+++ kmousetool/version.h
@@ -1,24 +1,3 @@
-/***************************************************************************
-                          version.h  -  KMouseTool Version Informatio
-                             -------------------
-    begin                : Sun Jan 20 23:27:58 PST 2002
-    copyright            : (C) 2002-2003 by Jeff Roush
-    email                : jeff@mousetool.com
-    copyright            : (C) 2003 by Olaf Schmidt
-    email                : ojschmidt@kde.org
-    copyright            : (C) 2003 by Gunnar Schmi Dt
-    email                : gunnar@schmi-dt.de
- ***************************************************************************/
-
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
-
 #ifndef kmousetool_version_h
 #define kmousetool_version_h
 
